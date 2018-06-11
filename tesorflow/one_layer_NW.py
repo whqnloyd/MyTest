556f246c42603b397ac845671b65d23ae3d765d9
whqnloyd <whqnloyd@users.noreply.github.com>
2018-06-11 12:55:35 -0400

    add CNN, modify 3 layers NN

--- tesorflow/one_layer_NW.py
+++ tesorflow/one_layer_NW.py
@@ -1,19 +1,22 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+
+#载入数据集
 mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
 
-#回归模型 y=softmax(wx+b)
-x = tf.placeholder(tf.float32, [None, 784])
+#设置模型
 w = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
+
+x = tf.placeholder(tf.float32, [None, 784])
 y1 = tf.nn.softmax(tf.matmul(x, w) + b)
+labels = tf.placeholder('float', [None, 10])
 
-#成本评估 y=-multi(y*log(y))
-y2 = tf.placeholder('float', [None, 10])
-cross_entropy = - tf.reduce_sum(y2 * tf.log(y1))
+#成本评估
+cross_entropy = - tf.reduce_sum(labels * tf.log(y1))
 
-#梯度下降 速率0.01 最小化成本
-train_step=tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+#梯度下降最小化成本
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
 #初始化向量
 init = tf.initialize_all_variables()
@@ -22,16 +25,15 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
-#循环训练
+#循环训练并评估
 for i in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y2: batch_ys})
-
-# 评估
-correct_prediction = tf.equal(tf.argmax(y1, 1), tf.argmax(y2, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-correct_rate = sess.run(accuracy, feed_dict={x: mnist.test.images, y2: mnist.test.labels})
-print('测试集正确率：%.2f' % (correct_rate * 100), '%')
+    batch_xs, batch_ys = mnist.train.next_batch(60)
+    sess.run(train_step, feed_dict={x: batch_xs, labels: batch_ys})
+    if (i % 100 == 0):
+        correct_prediction = tf.equal(tf.argmax(y1, 1), tf.argmax(labels, 1))
+        accu_set = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        correct_rate = sess.run(accu_set, feed_dict={x: mnist.test.images, labels: mnist.test.labels})
+        print('测试集准确率：%.2f' % (correct_rate * 100), '%')
 
 #保存训练好的模型
 model_path = 'model/one_layer'
