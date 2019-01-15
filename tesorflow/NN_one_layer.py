01619981f254c4d2e12d0c00996bd34a058125ff
whqnloyd <yangluodai@outlook.com>
2019-01-15 14:32:04 -0500

    GA

--- tesorflow/NN_one_layer.py
+++ tesorflow/NN_one_layer.py
@@ -8,43 +8,48 @@ mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
 # 设置模型
 w = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
-
 x = tf.placeholder(tf.float32, [None, 784])
-y1 = tf.nn.softmax(tf.matmul(x, w) + b)
-labels = tf.placeholder('float', [None, 10])
+y = tf.nn.softmax(tf.matmul(x, w) + b)
+labels = tf.placeholder(tf.float32, [None, 10])
 
 # 成本评估
-cross_entropy = - tf.reduce_sum(labels * tf.log(y1))
-
+loss = - tf.reduce_sum(labels * tf.log(y))
 # 梯度下降最小化成本
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+# 准确率计算
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(labels, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # 初始化向量
 init = tf.initialize_all_variables()
-
-# 设置并启动session
 sess = tf.Session()
 sess.run(init)
 
-detail_accuracy = []
-ys = []
+detail_accuracy_train = []
+detail_accuracy_test = []
+x_axis = []
 n = 0
+
 # 循环训练并评估
-for i in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(60)
-    sess.run(train_step, feed_dict={x: batch_xs, labels: batch_ys})
-    if i % 20 == 0:
-        correct_prediction = tf.equal(tf.argmax(y1, 1), tf.argmax(labels, 1))
-        accu_set = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        #correct_rate = sess.run(accu_set, feed_dict={x: mnist.test.images, labels: mnist.test.labels})
-        correct_rate = sess.run(accu_set, feed_dict={x: batch_xs, labels: batch_ys})
-        print('accuracy for test data：%.2f' % (correct_rate * 100), '%')
+for i in range(100):
+    batch_x_train, batch_label_train = mnist.train.next_batch(60)
+    sess.run(train_step, feed_dict={x: batch_x_train, labels: batch_label_train})
+    if i % 5 == 0:
+        correct_rate_train = sess.run(accuracy, feed_dict={x: batch_x_train, labels: batch_label_train})
+        print('accuracy for train data：%.2f' % (correct_rate_train * 100), '%')
+
+        batch_test = mnist.train.next_batch(1000)
+        correct_rate_test = sess.run(accuracy, feed_dict={x: batch_test[0], labels: batch_test[1]})
+        print('accuracy for test data：%.2f' % (correct_rate_test * 100), '%')
+
         n = n + 1
-        detail_accuracy.append(correct_rate * 100)
-        ys.append(n)
+        detail_accuracy_train.append(correct_rate_train * 100)
+        detail_accuracy_test.append(correct_rate_test * 100)
+        x_axis.append(n)
 
 plt.figure()
-plt.plot(ys, detail_accuracy)
+plt.plot(x_axis, detail_accuracy_train, color='red')
+plt.plot(x_axis, detail_accuracy_test, color='blue')
 plt.show()
 
 # 保存训练好的模型
