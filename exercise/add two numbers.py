6288f8ac1128c37e6837c6ec937d29fdff8c3d98
whqnloyd <yangluodai@outlook.com>
2019-03-09 13:04:14 -0500

    leetcode

--- exercise/add two numbers.py
+++ exercise/add two numbers.py
@@ -1,32 +1,45 @@
 def addTwoNumbers(self, l1, l2):
-    s = 0
-    l3 = []
+    a = l1
     while l1.next and l2.next:
-        l3.append((l1.val + l2.val + s) % 10)
-        s = int((l1.val + l2.val + s) / 10)
-        l2, l1 = l1.next, l2.next
-
-    l3.append((l1.val + l2.val + s) % 10)
-    s = int((l1.val + l2.val + s) / 10)
-
+        l1.val = l1.val + l2.val
+        if l1.val >= 10:
+            l1.val = l1.val - 10
+            l1.next.val = l1.next.val + 1
+        l1, l2 = l1.next, l2.next
     if l1.next:
-        l1 = l1.next
+        l1.val = l1.val + l2.val
+        if l1.val >= 10:
+            l1.val = l1.val - 10
+            l1.next.val = l1.next.val + 1
         while l1.next:
-            l3.append((l1.val + s) % 10)
-            s = int((l1.val + s) / 10)
             l1 = l1.next
-        l3.append((l1.val + s) % 10)
-        s = int((l1.val + s) / 10)
+            if l1.val >= 10:
+                l1.val = l1.val - 10
+                if l1.next:
+                    l1.next.val = l1.next.val + 1
+                else:
+                    break
+            else:
+                break
     elif l2.next:
-        l2 = l2.next
-        while l2.next:
-            l3.append((l2.val + s) % 10)
-            s = int((l2.val + s) / 10)
-            l2 = l2.next
-        l3.append((l2.val + s) % 10)
-        s = int((l2.val + s) / 10)
-
-    if s != 0:
-        l3.append(1)
-
-    return l3
+        l2.val = l1.val + l2.val
+        l1 = l2
+        if l1.val >= 10:
+            l1.val = l1.val - 10
+            l1.next.val = l1.next.val + 1
+        while l1.next:
+            l1 = l1.next
+            if l1.val >= 10:
+                l1.val = l1.val - 10
+                if l1.next:
+                    l1.next.val = l1.next.val + 1
+                else:
+                    break
+            else:
+                break
+    else:
+        l1.val = l1.val + l2.val
+    if l1.val >= 10:
+        l1.val = l1.val - 10
+        l1.next = ListNode(1)
+    return a
