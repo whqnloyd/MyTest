8ea587d3548ef7f7c68b6ab089f757ca4353068d
whqnloyd <yangluodai@outlook.com>
2019-08-08 10:52:27 -0400

    dll

--- c++workspace/c++workspace/gray-mix.h
+++ c++workspace/c++workspace/gray-mix.h
@@ -1,3 +1,8 @@
 #pragma once
+#ifdef GARY_MIX_EXPORTS
+#define GARY_MIX_API _declspec(dllexport)
+#else
+#define GARY_MIX_API _declspec(dllimport)
+#endif
 
-void GrayDepthSeperate();
+void GARY_MIX_API GrayDepthSeperate();
