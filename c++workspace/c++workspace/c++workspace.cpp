d92c181e9383b8e062927d236c80d45cab5b6a9e
whqnloyd <yangluodai@outlook.com>
2019-08-09 15:30:57 -0400

    renew keras

--- c++workspace/c++workspace/c++workspace.cpp
+++ c++workspace/c++workspace/c++workspace.cpp
@@ -1,5 +1,13 @@
-#include <gray-mix.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 void main() {
-	GrayDepthSeperate();
+	string info_file_path;
+	printf("information file path: ");
+	getline(cin, info_file_path);
+	if (info_file_path.size() == 0) {
+		printf("hellyeah\n");
+	}
 }
